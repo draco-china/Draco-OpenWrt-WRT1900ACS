515f8d4e629ca072454ef8ea1aec6a5b854ee3cc
Draco <Draco.coder@gmail.com>
2021-10-08 14:45:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,8 +14,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-bootstrap）
-sed -i 's/luci-theme-bootstrap/luci-theme-bootstrap/g' ./feeds/luci/collections/luci/Makefile
+# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-argon）
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 
 # 修复核心及添加温度显示
 sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
@@ -46,8 +46,6 @@ rm -rf ./openwrt-packages/luci-app-bypass
 rm -rf ./openwrt-packages/luci-app-passwall
 rm -rf ./openwrt-packages/luci-app-vssr
 rm -rf ./openwrt-packages/luci-app-ssr-plus
-rm -rf ./openwrt-packages/luci-app-adguardhome
-rm -rf ./openwrt-package/luci-app-argon-config
  
 # Add luci-app-passwall
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
@@ -61,9 +59,6 @@ rm -rf ./openwrt-package/luci-app-argon-config
 
 # Add ServerChan
 # git clone --depth=1 https://github.com/tty228/luci-app-serverchan
-
-# Add OpenClash
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 
 # Add luci-app-onliner
 # git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
@@ -79,18 +74,19 @@ rm -rf ../lean/luci-app-docker
 # git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 
 # Add luci-theme-argon
-rm -rf ../lean/luci-theme-argon
-rm -rf ../lean/luci-theme-bootstrap
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+rm -rf ../lean/luci-theme-argon
+rm -rf ../lean/luci-theme-bootstrap
+rm -rf ./openwrt-package/luci-app-argon-config
 
 # Add subconverter
 # git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
 
 # Add luci-udptools
-svn co https://github.com/zcy85611/Openwrt-Package/trunk/luci-udptools
-svn co https://github.com/zcy85611/Openwrt-Package/trunk/udp2raw
-svn co https://github.com/zcy85611/Openwrt-Package/trunk/udpspeeder-tunnel
+# svn co https://github.com/zcy85611/Openwrt-Package/trunk/luci-udptools
+# svn co https://github.com/zcy85611/Openwrt-Package/trunk/udp2raw
+# svn co https://github.com/zcy85611/Openwrt-Package/trunk/udpspeeder-tunnel
 
 # Add OpenAppFilter
 # git clone --depth 1 -b oaf-3.0.1 https://github.com/destan19/OpenAppFilter.git
@@ -98,13 +94,8 @@ svn co https://github.com/zcy85611/Openwrt-Package/trunk/udpspeeder-tunnel
 # Add luci-app-oled (R2S Only)
 # git clone --depth=1 https://github.com/NateLol/luci-app-oled
 
-# Add luci-app-adguardhome
-rm -rf ../lean/luci-app-adguardhome
-rm -rf ./luci-app-adguardhome
-svn co https://github.com/Hyy2001X/AutoBuild-Packages/trunk/luci-app-adguardhome
-
 # Add luci-app-xlnetacc
-# git clone --depth=1 https://github.com/sensec/luci-app-xlnetacc
+git clone --depth=1 https://github.com/sensec/luci-app-xlnetacc
 
 # Add apk (Apk Packages Manager)
 # svn co https://github.com/openwrt/packages/trunk/utils/apk
