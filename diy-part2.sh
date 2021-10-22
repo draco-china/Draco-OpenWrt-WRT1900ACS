1f81fbae440bfacb006a4ac6240cdbb3cb4809b1
Draco <draco.coder@gmail.com>
2021-10-22 16:24:41 +0800

    update config

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,19 +10,25 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-
-# Modify default IP
+# 修改openwrt登陆地址,把下面的 192.168.10.1 修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-argon）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+# 修改主机名字，把 DracoOpenWrt 修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i 's/OpenWrt/DracoOpenWrt/g' package/base-files/files/bin/config_generate
+
+# 修改 banner 文件（banner 文件在根目录）
+rm -rf package/base-files/files/etc/banner && cd .. && cp -f ./banner package/base-files/files/etc/ && cd openwrt
 
 # 修复核心及添加温度显示
 sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
-# 设置版本
-sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
+# Mod zzz-default-settings
+pushd package/lean/default-settings/files
+sed -i '/http/d' zzz-default-settings
+export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
+sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" zzz-default-settings
+popd
 
 # ttyd 自动登录
 sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${GITHUB_WORKSPACE}/openwrt/package/feeds/packages/ttyd/files/ttyd.config
@@ -37,26 +43,27 @@ mkdir package/community
 pushd package/community
 
 # Add Lienol's Packages
-git clone --depth=1 https://github.com/Lienol/openwrt-package
+# git clone --depth=1 https://github.com/Lienol/openwrt-package
+
+# Add luci-app-kodexplorer
+svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-ramfree
+rm -rf ../lean/luci-app-ramfree
+
+# Add luci-app-kodexplorer
+svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-kodexplorer
 rm -rf ../lean/luci-app-kodexplorer
 
-# Add kenzok8 Packages
-git clone --depth=1 https://github.com/kenzok8/openwrt-packages 
-rm -rf ./openwrt-packages/luci-app-vssr
-rm -rf ./openwrt-packages/luci-app-bypass
-rm -rf ./openwrt-packages/luci-app-passwall
-rm -rf ./openwrt-packages/luci-app-ssr-plus
-rm -rf ./openwrt-packages/luci-app-argon-config
-rm -rf ./openwrt-packages/luci-app-adguardhome
-rm -rf ./openwrt-packages/luci-app-smartdns
+# Add luci-app-smartdns
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-eqos
+rm -rf ../lean/luci-app-eqos
 
 # Add luci-app-adguardhome
-rm -rf ../lean/luci-app-adguardhome
 svn co https://github.com/Hyy2001X/AutoBuild-Packages/trunk/luci-app-adguardhome
+rm -rf ../lean/luci-app-adguardhome
 
-# Add luci-app-adguardhome
-rm -rf ../lean/luci-app-smartdns
+# Add luci-app-smartdns
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
+rm -rf ../lean/luci-app-smartdns
 
 # Add luci-app-passwall
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
@@ -67,6 +74,9 @@ svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
 
 # Add luci-proto-minieap
 git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
+
+# Add OpenClash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 
 # Add ServerChan
 git clone --depth=1 https://github.com/tty228/luci-app-serverchan
@@ -88,14 +98,13 @@ git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
 git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 
 # Add luci-theme-argon
-rm -rf ../lean/luci-theme-argon
-rm -rf ../lean/luci-theme-bootstrap
-rm -rf ./luci-theme-argon
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+rm -rf ../lean/luci-theme-argon
+rm -rf ../lean/luci-theme-bootstrap
 
 # Add subconverter
-git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
+# git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
 
 # Add luci-udptools
 svn co https://github.com/zcy85611/Openwrt-Package/trunk/luci-udptools
@@ -103,10 +112,10 @@ svn co https://github.com/zcy85611/Openwrt-Package/trunk/udp2raw
 svn co https://github.com/zcy85611/Openwrt-Package/trunk/udpspeeder-tunnel
 
 # Add OpenAppFilter
-git clone --depth 1 -b oaf-3.0.1 https://github.com/destan19/OpenAppFilter.git
+# git clone --depth 1 -b oaf-3.0.1 https://github.com/destan19/OpenAppFilter.git
 
 # Add luci-app-oled (R2S Only)
-git clone --depth=1 https://github.com/NateLol/luci-app-oled
+# git clone --depth=1 https://github.com/NateLol/luci-app-oled
 
 # Add extra wireless drivers
 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8812au-ac
@@ -131,9 +140,13 @@ rm -rf syncthing
 svn co https://github.com/openwrt/packages/trunk/utils/syncthing
 popd
 
+# Fix mt76 wireless driver
+pushd package/kernel/mt76
+sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\/modules.d\/mt76-usb' Makefile
+popd
+
 # patch
 pushd ${GITHUB_WORKSPACE}/openwrt
 cp -a ${GITHUB_WORKSPACE}/0003-upx-ucl-21.02.patch ${GITHUB_WORKSPACE}/openwrt
 cat 0003-upx-ucl-21.02.patch | patch -p1 > /dev/null 2>&1
 popd
-
