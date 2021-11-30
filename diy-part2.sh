d9a7990d1b06edbc599b51255e0c714787b537b3
Draco <draco.coder@gmail.com>
2021-11-30 21:03:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,20 +23,8 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci
 sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
-# 修改 banner 文件（banner 文件在根目录）
-pushd package/base-files/files/etc
-rm -rf ./banner && cp -a ${GITHUB_WORKSPACE}/banner .
-popd
-
-# Mod zzz-default-settings
-pushd package/lean/default-settings/files
-sed -i '/http/d' zzz-default-settings
-export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
-sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" zzz-default-settings
-popd
-
-# ttyd 自动登录
-sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${GITHUB_WORKSPACE}/openwrt/package/feeds/packages/ttyd/files/ttyd.config
+# Add OpenClash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 
 # Add luci-app-ssr-plus
 # pushd package/lean
@@ -48,49 +36,27 @@ mkdir package/community
 pushd package/community
 
 # Add Lienol's Packages
-# git clone --depth=1 https://github.com/Lienol/openwrt-package
-
-# Add luci-app-kodexplorer
-svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-ramfree
-rm -rf ../lean/luci-app-ramfree
-
-# Add luci-app-kodexplorer
-svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-kodexplorer
+git clone --depth=1 https://github.com/Lienol/openwrt-package
 rm -rf ../lean/luci-app-kodexplorer
 
-# Add luci-app-smartdns
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-eqos
-rm -rf ../lean/luci-app-eqos
-
-# Add luci-app-adguardhome
-svn co https://github.com/Hyy2001X/AutoBuild-Packages/trunk/luci-app-adguardhome
-rm -rf ../lean/luci-app-adguardhome
-
-# Add luci-app-smartdns
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
-rm -rf ../lean/luci-app-smartdns
-
 # Add luci-app-passwall
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
 # Add luci-app-vssr <M>
-# git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
-# git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
+git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
+git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
 
 # Add luci-proto-minieap
 git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
 
-# Add OpenClash
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash
-
 # Add ServerChan
 git clone --depth=1 https://github.com/tty228/luci-app-serverchan
 
+# Add OpenClash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+
 # Add luci-app-onliner
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
-
-# Add luci-app-xlnetacc
-git clone --depth=1 https://github.com/sensec/luci-app-xlnetacc
 
 # Add luci-app-diskman
 git clone --depth=1 https://github.com/SuLingGG/luci-app-diskman
@@ -98,9 +64,9 @@ mkdir parted
 cp luci-app-diskman/Parted.Makefile parted/Makefile
 
 # Add luci-app-dockerman
-# rm -rf ../lean/luci-app-docker
-# git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
-# git clone --depth=1 https://github.com/lisaac/luci-lib-docker
+rm -rf ../lean/luci-app-docker
+git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
+git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 
 # Add luci-theme-argon
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
@@ -109,18 +75,18 @@ rm -rf ../lean/luci-theme-argon
 rm -rf ../lean/luci-theme-bootstrap
 
 # Add subconverter
-# git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
+git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
 
 # Add luci-udptools
-# svn co https://github.com/zcy85611/Openwrt-Package/trunk/luci-udptools
-# svn co https://github.com/zcy85611/Openwrt-Package/trunk/udp2raw
-# svn co https://github.com/zcy85611/Openwrt-Package/trunk/udpspeeder-tunnel
+svn co https://github.com/zcy85611/Openwrt-Package/trunk/luci-udptools
+svn co https://github.com/zcy85611/Openwrt-Package/trunk/udp2raw
+svn co https://github.com/zcy85611/Openwrt-Package/trunk/udpspeeder-tunnel
 
 # Add OpenAppFilter
-# git clone --depth 1 -b oaf-3.0.1 https://github.com/destan19/OpenAppFilter.git
+git clone --depth 1 -b oaf-3.0.1 https://github.com/destan19/OpenAppFilter.git
 
 # Add luci-app-oled (R2S Only)
-# git clone --depth=1 https://github.com/NateLol/luci-app-oled
+git clone --depth=1 https://github.com/NateLol/luci-app-oled
 
 # Add extra wireless drivers
 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8812au-ac
@@ -150,8 +116,26 @@ pushd package/kernel/mt76
 sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\/modules.d\/mt76-usb' Makefile
 popd
 
+# 修改 banner 文件（banner 文件在根目录）
+pushd package/base-files/files/etc
+rm -rf ./banner && cp -a ${GITHUB_WORKSPACE}/banner .
+popd
+
+# ttyd 自动登录
+sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${GITHUB_WORKSPACE}/openwrt/package/feeds/packages/ttyd/files/ttyd.config
+
+# Mod zzz-default-settings
+pushd package/lean/default-settings/files
+sed -i '/http/d' zzz-default-settings
+export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
+sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" zzz-default-settings
+popd
+
 # patch
 pushd ${GITHUB_WORKSPACE}/openwrt
 cp -a ${GITHUB_WORKSPACE}/0003-upx-ucl-21.02.patch ${GITHUB_WORKSPACE}/openwrt
 cat 0003-upx-ucl-21.02.patch | patch -p1 > /dev/null 2>&1
 popd
+
+# Change default shell to zsh
+sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
